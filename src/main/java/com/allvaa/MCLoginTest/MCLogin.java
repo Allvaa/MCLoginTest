a640a1ac40acbd659d22c4b20994523212c4f35b
Alva <52526068+Allvaa@users.noreply.github.com>
2019-10-20 16:42:12 +0700

    Add files via upload

---
+++ src/main/java/com/allvaa/MCLoginTest/MCLogin.java
@@ -0,0 +1,61 @@
+package com.allvaa.MCLoginTest;
+
+import kong.unirest.HttpResponse;
+import kong.unirest.JsonNode;
+import kong.unirest.Unirest;
+import kong.unirest.json.JSONObject;
+
+import javax.swing.*;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+
+public class MCLogin {
+    private JPanel panel;
+    private JTextField accTextField;
+    private JButton loginButton;
+    private JLabel infoLabel;
+
+    public MCLogin() {
+        loginButton.addActionListener(new ActionListener() {
+            public void actionPerformed(ActionEvent e) {
+                infoLabel.setVisible(true);
+                String usernpass = accTextField.getText();
+                if (usernpass.equals("")) {
+                    infoLabel.setText("Please input the field");
+                    return;
+                }
+                String[] arr =  usernpass.split(":");
+                if (arr.length < 2) {
+                    infoLabel.setText("Missing email/username or password");
+                    return;
+                }
+
+                JSONObject agentObj = new JSONObject()
+                        .put("name", "Minecraft")
+                        .put("version", 1);
+                JSONObject obj = new JSONObject()
+                        .put("agent", agentObj)
+                        .put("username", arr[0])
+                        .put("password", arr[1])
+                        .put("requestUser", true);
+
+                HttpResponse<JsonNode> res = Unirest.post("https://authserver.mojang.com/authenticate")
+                        .header("Content-Type", "application/json")
+                        .body(obj)
+                        .asJson();
+
+                infoLabel.setText(res.getStatusText());
+            }
+        });
+    }
+
+    public static void main(String[] args) {
+        JFrame frame = new JFrame("Minecraft Login Test");
+        frame.setContentPane(new MCLogin().panel);
+        frame.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+        frame.setSize(500, 300);
+        frame.setLocationRelativeTo(null);
+        frame.setResizable(false);
+        frame.setVisible(true);
+    }
+}
